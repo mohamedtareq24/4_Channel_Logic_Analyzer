8229dc50841e6704de61507fd5f51af194515bfc
Mohamed Tarek <90535558+mohamedtareq24@users.noreply.github.com>
2022-08-06 19:55:33 +0200

    Delete DATA_PATH directory

--- DATA_PATH/RAM.v
+++
@@ -1,9 +0,0 @@
-module RAM 
-    (
-	input [13:0] addr, 
-	input  data, we, clk,
-	output [13:0] addr_reg,
-	output  q
-);
-single_port_ram MEMORY (addr,data,we,clk,addr_reg,q);
-endmodule //RAM
--- DATA_PATH/WA_CTRL.v
+++
@@ -1,7 +0,0 @@
-module WA_CTRL (
-  input clk ,we, 
-  input  [13:0] old_address, 
-  output [13:0] write_address
-);
-assign write_address = old_address + we ;
-endmodule //WA_CTRL
